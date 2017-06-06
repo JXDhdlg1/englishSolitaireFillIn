0df2a1c2d8849e169d45fe0171af98af923457c7
jiangxud <18817337394@163.com>
2017-06-06 17:53:10 +0800

    save

---
+++ php-dev/thinkphp3.2-dev/Application/Home/Conf/config.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+    //'配置项'=>'配置值'
+    'URL_CASE INSENSITIVE' =>true,
+    "DB_DEPLOY_TYPE"=>1, //是否启用分布式
+    //数据库配置
+    'DB_RW_SEPARATE'=>false, //是否启用智能读写分离
+    'DB_TYPE'      =>  'mysql',     // 数据库类型
+    'DB_HOST'      =>  '106.14.60.210',     // 服务器地址
+    'DB_NAME'      =>  'english_game',     // 数据库名
+    'DB_USER'      =>  'guest',     // 用户名
+    'DB_PWD'       =>  'Guestisno2!',     // 输入安装MySQL时设置的密码
+    'DB_PORT'      =>  '3306',     // 端口
+    'DB_PREFIX'    =>  '',     // 数据库表前缀
+    'DB_DSN'       =>  '',     // 数据库连接DSN 用于PDO方式
+    'CACHE_OPEN'=>false,
+    'APP_USE_REDIS' => false,//是否开启哨兵redis缓存 ；前提是Common / Config / config.php APP_USE_REDIS_SENTINEL 支持 才生效
+    //测试
+    'TEST_VALUE' => "qwerttyui123",
+);
---
+++ php-dev/thinkphp3.2-dev/Application/Home/Model/EnglishWordModel.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jiangxudong
+ * Date: 2017/6/6
+ * Time: 17:45
+ */
+
+use Think\Model;
+
+class EnglishWordModel extends Model
+{
+    protected  $tableName = "english_word";
+
+    public function test() {
+        return "test";
+    }
+
+    public function getWords() {
+        $data = $this->where(array())->limit(1)->select();
+        return $data;
+    }
+
+}
