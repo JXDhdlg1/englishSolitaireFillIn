715f6187bf0c1092953f44b7632e181eacdd8e66
jiangxudong <jiangxudong@qiyi.com>
2017-06-08 22:38:57 +0800

    zancun

--- php-dev/thinkphp3.2-dev/Application/Home/Controller/IndexController.class.php
+++ php-dev/thinkphp3.2-dev/Application/Home/Controller/IndexController.class.php
@@ -5,7 +5,7 @@ use Think\Controller;
 class IndexController extends BaseController {
 
     public function index(){
-	$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
 
     public function showWord() {
@@ -13,7 +13,7 @@ class IndexController extends BaseController {
         if(empty($user)) {
             $this->redirect("/Home/user/login",array(),3,"请先登录");
         }
-        
+
         $this->userName = $user['userName'];
 
     }
